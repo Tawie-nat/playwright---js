bb224e6c66897562d09a2cad34f518c2fb23a845
Tawie <tawie@mail.ru>
2022-06-01 15:32:46 +0300

    Netology tests

---
+++ tests/App.spec.js
@@ -0,0 +1,71 @@
+const { test, expect } = require("@playwright/test");
+const { chromium } = require("playwright");
+const { email, pass } = require("./user");
+
+test.describe('Authorization', () => { 
+  test("successfulAutorization", async () => {
+    const browser = await chromium.launch({
+      headless: false,
+      slowMo: 2000});
+    const page = await browser.newPage();
+    await page.goto('https://netology.ru/?modal=sign_in');
+    
+  
+    // Click [placeholder="Email"]
+    await page.locator('[placeholder="Email"]').click();
+    // Fill [placeholder="Email"]
+    await  page.locator('[placeholder="Email"]').fill(user.email);
+ 
+    // Click [placeholder="Пароль"]
+    await page.locator('[placeholder="Пароль"]').click();
+    // Fill [placeholder="Пароль"]
+    await page.locator('[placeholder="Пароль"]').fill(user.pass);
+
+    // Press Enter
+    await Promise.all([
+      page.waitForNavigation(/*{ url: 'https://netology.ru/profile' }*/),
+      page.locator('[placeholder="Пароль"]').press('Enter')
+    ]);
+
+    expect(page.locator('text=Мои курсы и профессии Расписание занятий')).toBeVisible;
+    await page.pause();
+    await page.screenshot({
+      path: './screenshots/Test1.png',
+    });
+    await page.close();
+  });
+
+
+  test("unsuccessfulAutorization", async () => {
+    const browser = await chromium.launch({
+      headless: false,
+      slowMo: 2000});
+    const page = await browser.newPage();
+    await page.goto('https://netology.ru/?modal=sign_in');
+    
+
+    // Click [placeholder="Email"]
+    await page.locator('[placeholder="Email"]').click();
+    // Fill [placeholder="Email"]
+    await  page.locator('[placeholder="Email"]').fill(user.email);
+
+    // Click [placeholder="Пароль"]
+    await page.locator('[placeholder="Пароль"]').click();
+    // Fill [placeholder="Пароль"]
+    await page.locator('[placeholder="Пароль"]').fill('12345');
+
+    // Press Enter
+    await Promise.all([
+    page.locator('[placeholder="Пароль"]').press('Enter')
+    ]);
+
+    expect(page.locator('[data-testid="login-error-hint"]')).toBeVisible;
+    await page.pause();
+    await page.screenshot({
+      path: './screenshots/Test2.png',
+  });
+    await page.close();
+  });
+
+
+})
---
+++ tests/user.js
@@ -0,0 +1,2 @@
+export const email = "7_jeka_7@mail.ru";
+export const pass = "787898077";
